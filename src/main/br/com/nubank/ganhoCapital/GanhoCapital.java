235200a606ed723a52bc333c010e33aa56bfba74
Renato Fagalde <renato@likwi.com.br>
2023-11-18 21:05:03 -0300

    prepare input, the input is not a regular json list

--- src/main/br/com/nubank/ganhoCapital/GanhoCapital.java
+++ src/main/br/com/nubank/ganhoCapital/GanhoCapital.java
@@ -2,8 +2,11 @@ package br.com.nubank.ganhoCapital;
 
 import br.com.nubank.ganhoCapital.model.OperationInput;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonStreamParser;
 import jdk.dynalink.Operation;
 
@@ -11,6 +14,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GanhoCapital {
 
@@ -18,39 +23,64 @@ public class GanhoCapital {
     public static final String DELIMITADOR_ARRAY_OBJETO = ",";
     public static final String DELIMITADOR_ARRAY_LIST = "]";
 
+
     //todo validar exception JsonProcessingException
     public static void main(String[] args) throws JsonProcessingException {
         Scanner scanner = new Scanner(System.in);
+        //List<OperationInput> operationsInput = new ArrayList<>();
 
 //        String inputStringList = getInputList(scanner);
-        String inputStringList="[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000},\n" +
-                "{\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 5000}]\n" +
-                "[{\"operation\":\"buy\", \"unit-cost\":20.00, \"quantity\": 10000},\n" +
-                "{\"operation\":\"sell\", \"unit-cost\":10.00, \"quantity\": 5000}]";
-        inputStringList=inputStringList.replaceAll("\n","");
-        List<OperationInput> operationsInput = new ArrayList<>();
+//        String inputStringList = "[{\"operation\":\"buy\", \"unit\":10.00, \"quantity\": 100},\n" +
+//                "{\"operation\":\"sell\", \"unit\":15.00, \"quantity\": 50},\n" +
+//                "{\"operation\":\"sell\", \"unit\":15.00, \"quantity\": 50}]";
 
-        fixMultipleRootListInString(inputStringList);
+//        String inputStringList = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000},\n" +
+//                "{\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 5000}]\n" +
+//                "[{\"operation\":\"buy\", \"unit-cost\":20.00, \"quantity\": 10000},\n" +
+//                "{\"operation\":\"sell\", \"unit-cost\":10.00, \"quantity\": 5000}]";
+        String inputStringList = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100},\n" +
+                "{\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50},\n" +
+                "{\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}]\n" +
+                "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000},\n" +
+                "{\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 5000},\n" +
+                "{\"operation\":\"sell\", \"unit-cost\":5.00, \"quantity\": 5000}]";
+        inputStringList = inputStringList.replaceAll("\n", "");
 
-        System.out.println("inputStringList = " + inputStringList);
+        inputStringList = fixMultipleRootListInString(inputStringList);
+        List<List<OperationInput>> allListOperations = converterToListOperation(inputStringList);
+        allListOperations.stream().forEach(listOperation->{
+            listOperation.stream().forEach(operationInput -> {
+                System.out.println("operationInput.getOperation() = " + operationInput.getOperation() + "  " +operationInput.getUnitCost() + "  "+operationInput.getQuantity());
+//                System.out.println("operationInput.getUnitCost() = " + operationInput.getUnitCost());
+//                System.out.println("operationInput.getQuantity() = " + operationInput.getQuantity());
+            });
+            System.out.println("----------------");
+        });
+                
+
     }
 
-    private static void fixMultipleRootListInString(String inputStringList) {
+    private static List<List<OperationInput>> converterToListOperation(String inputStringList) throws JsonProcessingException {
+        String[] jsons = inputStringList.split("#");
+        List<List<OperationInput>> operationInputsAll = new ArrayList<>();
+        for(String s:jsons){
+            List<OperationInput> operationInputs= objectMapper.readValue(s, new TypeReference<List<OperationInput>>() {
+            });
 
-        inputStringList = inputStringList.replaceAll("]\\[","],[");
-        //change to list with lists
-        inputStringList = String.format("%s,%s,%s","[",inputStringList,"]");
+            operationInputsAll.add(operationInputs);
+        }
+        return operationInputsAll;
+    }
 
-        System.out.println("inputStringList = " + inputStringList);
-
-//        return inputStringList;
+    private static String fixMultipleRootListInString(String inputStringList) {
+        return inputStringList.replaceAll("]\\[", "]#[");
     }
 
     private static String getInputList(Scanner scanner) {
         StringBuffer inputLists = new StringBuffer();
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
-            input = input.replaceAll("\n","");
+            input = input.replaceAll("\n", "");
             if (input.isEmpty()) break;
             inputLists.append(input);
         }
